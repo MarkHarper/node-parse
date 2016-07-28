a172c66736e8e3403289f4f7c0a2e5fa232a72c3
Mark Harper <markharper20@gmail.com>
2016-07-28 16:51:10 -0400

    almost

---
+++ app/csv-parse.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+
+let dataSet = {};
+let types = {};
+
+fs.createReadStream('../data/test-contacts.csv')
+    .pipe(csv())
+    .on('headers', setHeaders)
+    .on('data', flatten)
+    .on('end', function (data) {
+        identifyDateType(dataSet);
+        checkForMc(dataSet);
+        checkForText(dataSet);
+        console.log(types);
+    });
+
+function setHeaders (headers) {
+    headers.forEach(function(element) {
+        if (element.length !== 0) {
+            types[element] = undefined;
+            dataSet[element] = [];
+        }
+    });
+}
+
+function flatten (data) {
+    for (header in dataSet) {
+        dataSet[header].push(data[header]);
+    }
+}
+
+function identifyDateType (data) {
+    for (category in data) {
+        var x = 0;
+        data[category].forEach(function(item) {
+            if (new Date(item).toString() !== 'Invalid Date') {
+                x++;
+            }
+        });
+        if (x > data[category].length/2) {
+            types[category] = {type: 'Date/Time' };
+        }
+    }
+}
+
+function checkForText (data) {
+    for (category in data) {
+        let stringCount = 0;
+        data[category].forEach(function(item) {
+            if (typeof data[item] === 'string' && data[item].match(/"^[a-zA-Z0-9_]*$"/)) {
+                stringCount++;
+            }
+        });
+        if (stringCount < data[category].length/2 && types[category] === undefined) {
+            types[category] = { type: 'Text'};
+        }
+    }
+}
+
+function checkForMc (data) {
+    for (category in data) {
+        let options = {};
+        data[category].forEach(function(item) {
+            if (options[item]) {
+                options[item]++;
+            } else {
+                options[item] = 1;
+            }
+        });
+        if (Object.keys(options).length < data[category].length/2 && types[category] === undefined) {
+            types[category] = { type: 'Multiple Choice', options: Object.keys(options)};
+        }
+    }
+}
+ 
---
+++ test/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+
+describe('maybeFirst', function() {
+  it('returns the first element of an array', function() {
+    var result = 1;
+
+    assert.equal(result, 1, 'maybeFirst([1, 2, 3]) is 1');
+  });
+});
